aebd5000f540b0526190d61768dffb06c1364ad4
thiago-menezes <thiago.menezes@gruposbf.com.br>
2022-03-15 23:20:55 -0300

    deleting to test

--- src/api/vaga/controllers/vaga.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  vaga controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::vaga.vaga');
--- src/api/vaga/routes/vaga.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * vaga router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::vaga.vaga');
