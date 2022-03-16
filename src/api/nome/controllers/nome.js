c02001168c4f8eed69b04a950e6a680cdf5f9c28
thiago-menezes <thiago.menezes@gruposbf.com.br>
2022-03-15 23:58:23 -0300

    teste

---
+++ src/api/nome/controllers/nome.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  nome controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::nome.nome');
---
+++ src/api/nome/services/nome.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nome service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::nome.nome');
