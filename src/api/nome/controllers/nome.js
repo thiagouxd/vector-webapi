e95782db0b8fb053b795e37e2ef20c77e058e170
thiago-menezes <thiago.menezes@gruposbf.com.br>
2022-03-16 13:16:26 -0300

    adjustiments SEO

--- src/api/nome/controllers/nome.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  nome controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::nome.nome');
--- src/api/nome/services/nome.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nome service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::nome.nome');
